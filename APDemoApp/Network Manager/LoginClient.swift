c4a959565f144a22974f03565939bd7a4f5e06a9
dennisvera <dennisvera.z@gmail.com>
2017-12-13 23:00:27 -0500

    login request not successful

---
+++ APDemoApp/Network Manager/LoginClient.swift
@@ -0,0 +1,14 @@
+//
+//  LoginClient.swift
+//  APDemoApp
+//
+//  Created by Dennis Vera on 12/13/17.
+//  Copyright © 2017 Dennis Vera. All rights reserved.
+//
+
+import Foundation
+
+class LoginClient {
+    
+    
+}
--- APDemoApp/View Controllers/LoginViewController/LoginViewController.swift
+++ APDemoApp/View Controllers/LoginViewController/LoginViewController.swift
@@ -27,23 +27,79 @@ class LoginViewController: UIViewController {
     }
     
     // MARK: -
-
+    
     func setUpViews() {
         userNameTextField.setRoundedBorder()
         passwordTextField.setRoundedBorder()
-        
         loginButton.layer.cornerRadius = 20
     }
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    @IBAction func didPressLoginButton(_ sender: Any) {
+        
+        guard let userName = userNameTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        //declare parameter as a dictionary which contains string as key and value combination. considering inputs are valid
+        
+        let parameters = ["username": userName, "password": password]
+        
+        //create the url with URL
+        guard let url = URL(string: "http://dev3.apppartner.com/AppPartnerDeveloperTest/scripts/login.php") else {return}
+        
+        //create the session object
+        let session = URLSession.shared
+        
+        //now create the URLRequest object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                    // handle json...
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        
+        task.resume()
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
